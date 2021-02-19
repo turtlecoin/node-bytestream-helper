440ef5688d360a18bca5254ee076c82e1d05b42f
Brandon Lehmann <brandonlehmann@gmail.com>
2021-02-19 11:13:03 -0500

    - Bump Dependencies - Bump copyright year(s) - Bump version - Rebuild documentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-// Copyright (c) 2018-2020, The TurtleCoin Developers
+// Copyright (c) 2018-2021, The TurtleCoin Developers
 //
 // Please see the included LICENSE file for more information.
+
+import * as BigInteger from 'big-integer';
 
 export { Reader } from './reader';
 export { Writer } from './writer';
 export { Varint } from './varint';
+export { BigInteger };
